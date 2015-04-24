8e574da7b36aa4fb4a0b3cea0b64486444a56536
Jayasurian Makkoth <jmakkoth@babbel.com>
2015-04-24 16:01:02 +0200

    - added getbyIndex

--- src/js/stores/timeboxer_template_store.js
+++ src/js/stores/timeboxer_template_store.js
@@ -3,53 +3,22 @@ var Dispatcher = require('flux-riot').Dispatcher;
 var ActionTypes = require('../constants/timeboxer_constants.js').ActionTypes;
 var flux_riot = require('flux-riot');
 
-var _templates = [{
-  "name": "Hackday 2014 Time Boxer Presentation",
-  "agenda": [
-    {
-      "name": "Short Brief",
-      "time": "2"
-    }, {
-      "name": "Presentation of Details",
-      "time": "5"
-    }, {
-      "name": "Summary",
-      "time": "2"
-    }, {
-      "name": "Feedback",
-      "time": "0.5"
-    }
-  ]
-}, {
-  "name": "Hackday 2014 Time Boxer Presentation 2",
-  "agenda": [
-    {
-      "name": "Short Brief",
-      "time": "2"
-    }, {
-      "name": "Presentation of Details",
-      "time": "5"
-    }, {
-      "name": "Summary",
-      "time": "2"
-    }, {
-      "name": "Feedback",
-      "time": "0.5"
-    }
-  ]
-}];
+var _templates = [];
 
 var getTemplates = function () {
   return _templates;
 };
 var saveTemplates = function (obj) {
   _templates = JSON.parse(obj[0].attributes.hack_sample);
-}
+};
 
 TimeboxerTemplateStore = assign(new flux_riot.BaseStore(), {
   getAll: function () {
     console.log(getTemplates());
     return getTemplates();
+  },
+  getByIndex: function (index) {
+    return _templates[index];
   },
   dispatchToken: Dispatcher.register(function(payload) {
     var action, data, index, task;
